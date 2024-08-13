ba4e1f09b9ffee6c288e55bb15fae7d8b3f8e036
yuki731 <76100749+yuki731@users.noreply.github.com>
2024-08-13 21:37:20 +0900

    Revert "create signup and signin api"

--- api/bank/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserSignupView, signin
-
-urlpatterns = [
-    path('signup/', UserSignupView.as_view(), name='signup'),
-    path('signin/', signin, name='signin'),
-]
