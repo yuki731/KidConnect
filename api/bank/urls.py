51f9ec9866ad19418f8b604d5332f6f2f8dec7dd
yuki731 <yuki731ymd@outlook.com>
2024-08-15 21:12:36 +0900

    create children view

--- api/bank/urls.py
+++ api/bank/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserSignupView, signin, UserDetailView, ChildrenInFamilyView, ChildPocketMoneyView
+from .views import UserSignupView, signin, UserDetailView, ChildrenInFamilyView, ChildPocketMoneyView, CreateJobCardView, ApprovalJobRequestView, ApprovalWithdrawalRequestView, ChildDashboardView, TaskView, ReportJobView, CreateWithdrawalRequestView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -10,4 +10,12 @@ urlpatterns = [
     path('user/', UserDetailView.as_view(), name='user-detail'),
     path('family/children/', ChildrenInFamilyView.as_view(), name='children_in_family'),
     path('child/<int:child_id>/pocket-money/', ChildPocketMoneyView.as_view(), name='child_pocket_money'),
+    path('child/<int:child_id>/pocket-money/<int:job_card_id>/', ChildPocketMoneyView.as_view(), name='child_pocket_money_delete'),
+    path('create-job-card/', CreateJobCardView.as_view(), name='create_job_card'),
+    path('approval-job-request/<int:job_report_id>/', ApprovalJobRequestView.as_view(), name='approval_job_request'),
+    path('approval-withdrawal-request/<int:withdrawal_request_id>/', ApprovalWithdrawalRequestView.as_view(), name='approval_withdrawal_request'),
+    path('child-dashboard/', ChildDashboardView.as_view(), name='child_dashboard'),
+    path('task-view/', TaskView.as_view(), name='task_view'),
+    path('report-job/<int:job_id>/', ReportJobView.as_view(), name='report_job'),
+    path('create-withdrawal-request/', CreateWithdrawalRequestView.as_view(), name='create_withdrawal_request'),
 ]
--- api/bank/views.py
+++ api/bank/views.py
@@ -1,15 +1,21 @@
-from rest_framework import status
-from rest_framework import generics
+from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import Group
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.db.models import Sum, Case, When, F, DecimalField, IntegerField
 
-from .models import CustomUser, PocketMoney
+
+from .models import PocketMoney, JobCard, JobReport, WithdrawalRequest
 from .serializers import UserSignupSerializer, LoginSerializer, CreateUserSerializer, UserSerializer, PocketMoneySerializer, JobCardSerializer, JobReportSerializer, WithdrawalRequestSerializer
+
+User = get_user_model()
 
 class UserSignupView(APIView):
     def post(self, request, *args, **kwargs):
@@ -33,6 +39,8 @@ def signin(request):
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token}, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+############# Parents Views ##############
 
 class UserDetailView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
@@ -107,11 +115,10 @@ class ChildrenInFamilyView(generics.ListAPIView):
         if not user.groups.filter(name='Parents').exists():
             return User.objects.none()
 
-        group_name = None
-        groups = parent_user.groups.all()
-        for group in groups:
-            if group.name.startswith(user.family_name):
-                group_name = group.name
+        family_group = None
+        for group in user.groups.all():
+            if group.name != 'Parents':
+                family_group = group
                 break
 
         if not family_group:
@@ -153,3 +160,159 @@ class ChildPocketMoneyView(generics.RetrieveAPIView):
             'job_reports': JobReportSerializer(job_reports, many=True).data,
             'withdrawal_requests': WithdrawalRequestSerializer(withdrawal_requests, many=True).data,
         })
+    
+    def delete(self, request, *args, **kwargs):
+        child = self.get_object()
+        job_card_id = self.kwargs.get('job_card_id')
+        
+        job_card = get_object_or_404(JobCard, id=job_card_id, child=child)
+        job_card.delete()
+    
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CreateJobCardView(generics.CreateAPIView):
+    serializer_class = JobCardSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.groups.filter(name='Parents').exists():
+            return Response({"detail": "Not authorized."}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            children = serializer.validated_data.get('children')
+            job_name = serializer.validated_data.get('job_name')
+            money = serializer.validated_data.get('money')
+            job_image = serializer.validated_data.get('job_image')
+
+            family_group = request.user.groups.exclude(name='Parents').first()
+
+            for child in children:
+                JobCard.objects.create(
+                    child=child,
+                    group=family_group.name,
+                    job_name=job_name,
+                    money=money,
+                    job_image=job_image
+                )
+            
+            return Response({"detail": "Job cards created successfully."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ApprovalJobRequestView(generics.UpdateAPIView):
+    serializer_class = JobReportSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.groups.filter(name='Parents').exists():
+            return Response({"detail": "Not authorized."}, status=status.HTTP_403_FORBIDDEN)
+        
+        job_report_id = self.kwargs.get('job_report_id')
+        job_report = get_object_or_404(JobReport, id=job_report_id)
+
+        PocketMoney.objects.create(
+            child=job_report.reported_by,
+            group=job_report.group,
+            amount=job_report.money,
+            date=timezone.now().date(),
+            transaction_type=PocketMoney.DEPOSIT,
+            memo=job_report.job_name
+        )
+
+        job_report.delete()
+
+        return Response({"detail": "Job report approved successfully."}, status=status.HTTP_200_OK)
+
+
+class ApprovalWithdrawalRequestView(generics.UpdateAPIView):
+    serializer_class = WithdrawalRequestSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.groups.filter(name='Parents').exists():
+            return Response({"detail": "Not authorized."}, status=status.HTTP_403_FORBIDDEN)
+        
+        withdrawal_request_id = self.kwargs.get('withdrawal_request_id')
+        withdrawal_request = get_object_or_404(WithdrawalRequest, id=withdrawal_request_id)
+
+        PocketMoney.objects.create(
+            child=withdrawal_request.reported_by,
+            group=withdrawal_request.group,
+            amount=withdrawal_request.money,
+            date=timezone.now().date(),
+            transaction_type=PocketMoney.WITHDRAWAL,
+            memo=withdrawal_request.title
+        )
+
+        withdrawal_request.delete()
+
+        return Response({"detail": "Withdrawal request approved successfully."}, status=status.HTTP_200_OK)
+    
+############# Children Views ##############
+
+class ChildDashboardView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Parents').exists():
+            return Response({"detail": "Not authorized."}, status=status.HTTP_403_FORBIDDEN)
+
+        child = request.user
+        pocket_money_records = PocketMoney.objects.filter(child=child)
+        total_amount = pocket_money_records.aggregate(total=Sum(
+            Case(
+                When(transaction_type=PocketMoney.DEPOSIT, then='amount'),
+                When(transaction_type=PocketMoney.WITHDRAWAL, then=-1 * F('amount')),
+                output_field=IntegerField()
+            )
+        ))['total']
+
+        return Response({'total_amount': total_amount})
+
+class TaskView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        child = request.user
+        job_cards = JobCard.objects.filter(child=child)
+        serializer = JobCardSerializer(job_cards, many=True)
+        return Response({'job_cards': serializer.data})
+
+class ReportJobView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        job_id = self.kwargs.get('job_id')
+        job = get_object_or_404(JobCard, id=job_id)
+        user = request.user
+
+        family_group = None
+        for group in user.groups.all():
+            if group.name != 'Children':
+                family_group = group
+
+        JobReport.objects.create(
+            job_name=job.job_name,
+            money=job.money,
+            group=family_group,
+            reported_by=user,
+        )
+
+        return Response({"detail": f"Job {job.job_name} has been reported for approval."}, status=status.HTTP_201_CREATED)
+
+class CreateWithdrawalRequestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        family_group = None
+        for group in user.groups.all():
+            if group.name != 'Parents':
+                family_group = group
+
+        serializer = WithdrawalRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            withdrawal_request = serializer.save(reported_by=user, group=family_group)
+            return Response({'detail': 'Withdrawal request created successfully.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
